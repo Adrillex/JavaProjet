f9cf838147c84c09b70358cfa558a37d9be72beb
Vincent Dugard <vincent.dugard@viacesi.fr>
2016-06-15 10:32:03 +0200

    Ajout des arraylist et renommage de bourse

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import contract.IModel;
@@ -17,10 +19,48 @@ public class Model implements IModel {
 	public Model() {
 	}
 	
-	public void LoadStage(int Stage){
+	private void LoadStage(int Stage){
+		ArrayList <Wall> vWallList = new ArrayList <Wall>();
+		ArrayList <Wall> hWallList = new ArrayList <Wall>();
+		ArrayList <Wall> rWallList = new ArrayList <Wall>();
+		ArrayList <CrystalBall> cBallList = new ArrayList <CrystalBall>();
+		ArrayList <Purse> purseList = new ArrayList <Purse>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		ArrayList <Wall> wallList = new ArrayList <Wall>();
+		
+		int numStage = 1;
+		DBConnection instance = DBConnection.getInstance();
+		final String sql = "{call VoirStage" + numStage + "}";
+		final CallableStatement call = instance.getConnection().prepareCall(sql);
+		call.execute();
+		final ResultSet rs = call.getResultSet();
+		
+		while (rs.next()){
+			switch (rs.getInt(id)) {
+			case 1:
+				// horizontal wall
+				
+				break;
+			case 2:
+				// vertical wall
+				break;
+			case 3:
+				// round wall
+				break;
+			
+			default:
+				break;
+			}
+		}
+		
 	}
 	
-	private ArrayList<Element> getElement(){
+	private ArrayList<Mobile> getMobile(){
 		return null;
 	}
 	
--- model/src/main/java/model/Purse.java
+++ model/src/main/java/model/Purse.java
@@ -2,12 +2,12 @@ package model;
 
 import java.awt.Image;
 
-public class Bonus extends Fixe{
+public class Purse extends Fixe{
 	
 	private Position position;
 	private Image image;
 	
-	public Bonus(Position position, Image image){
+	public Purse(Position position, Image image){
 		this.position = position;
 		this.image = image;
 	}
