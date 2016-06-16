e07066f468f823c8f65f05386edbd21ac966e14b
Thomas Adonta <thomas.adonta@viacesi.fr>
2016-06-16 09:37:30 +0200

    Ajout IElement

---
+++ contract/src/main/java/contract/IElement.java
@@ -0,0 +1,16 @@
+package contract;
+
+public interface IElement {
+	
+	public int getPosX(){
+		return posX;
+	}
+	
+	public int getPosY(){
+		return posY;
+	}
+	
+	public BufferedImage getSprite(){
+		return BufferedImage;
+	}
+}
